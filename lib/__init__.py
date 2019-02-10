75446105eaf1e0c64a124c039a18f2ee55a1103f
Prometeo AI <47270205+prometeobot@users.noreply.github.com>
2019-02-10 11:32:28 +0000

    Create __init__.py

---
+++ lib/__init__.py
@@ -0,0 +1,7 @@
+__title__ = "Prometeo AI Base library"
+__version__ = "0.0.1"
+__license__ = "MIT License"
+__copyright__ = "Copyright (c) Prometeo AI, Inc. All rights reserved."
+
+# Version synonym
+VERSION = __version__
